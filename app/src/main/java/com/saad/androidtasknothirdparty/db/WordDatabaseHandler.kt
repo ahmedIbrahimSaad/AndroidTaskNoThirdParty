0e3f3c4f1430792978f17e252ee41dfcf94bb16f
Ahmed Saad <asibrahim@ntgclarity.com>
2020-11-21 17:41:05 +0200

    add sqlite database handler and control

---
+++ app/src/main/java/com/saad/androidtasknothirdparty/db/WordDatabaseHandler.kt
@@ -0,0 +1,22 @@
+package com.saad.parsexmlbyhttpclientsqlliteapp.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+const val SCHEMA_NAME="WORDDATABASE"
+const val INSERT="CREATE TABLE words( id INTEGER PRIMARY KEY AUTOINCREMENT,word TEXT,occurrency INTEGER ) "
+const val DROP="DROP TABLE IF EXISTS words"
+
+open class WordDatabaseHandler(val context: Context): SQLiteOpenHelper(context, SCHEMA_NAME, null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db!!.execSQL(INSERT)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL(DROP)
+        onCreate(db)
+    }
+
+}
---
+++ app/src/main/java/com/saad/androidtasknothirdparty/db/WordsControl.kt
@@ -0,0 +1,44 @@
+package com.saad.parsexmlbyhttpclientsqlliteapp.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+
+const val KEY="word"
+const val VALUE="occurrency"
+const val TABLE_NAME="words"
+const val ID="id"
+class WordsControl(context: Context): WordDatabaseHandler(context) {
+
+    fun create(occurency: MutableMap<String, Int>?): Boolean {
+        val values = ContentValues()
+        val db: SQLiteDatabase = this.getWritableDatabase()
+        for ((key, value) in occurency!!.entries) {
+            values.put(KEY, key)
+            values.put(VALUE, value)
+            db.insert(TABLE_NAME, null, values)
+        }
+
+        db.close()
+        return true
+    }
+
+    fun read(): MutableMap<String,Int>? {
+        var recordsList: MutableMap<String,Int> = HashMap<String, Int>()
+        val sql = "SELECT * FROM words ORDER BY id DESC"
+        val db = this.writableDatabase
+        val cursor: Cursor = db.rawQuery(sql, null)
+        if (cursor.moveToFirst()) {
+            do {
+                val id: Int = cursor.getString(cursor.getColumnIndex(ID)).toInt()
+                val word: String = cursor.getString(cursor.getColumnIndex(KEY))
+                val occurrency = cursor.getInt(cursor.getColumnIndex(VALUE))
+                recordsList.put(word,occurrency)
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        db.close()
+        return recordsList
+    }
+}
