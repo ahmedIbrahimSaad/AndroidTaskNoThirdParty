b3e13ac7d8c11d00402ce7c2878bf9735fd24a14
Ahmed Saad <asibrahim@ntgclarity.com>
2020-11-21 21:19:52 +0200

    add toast for network type

--- app/src/main/java/com/saad/androidtasknothirdparty/presentation/MainViewModel.kt
+++ app/src/main/java/com/saad/androidtasknothirdparty/presentation/MainViewModel.kt
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.NetworkInfo
 import android.os.Build
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.saad.androidtasknothirdparty.di.Injector
@@ -14,35 +15,74 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    private var occurrences  = HashMap<String, Int>()
-    private var liveOccurrences= MutableLiveData<MutableMap<String, Int>>()
+    private var occurrences = HashMap<String, Int>()
+    private var liveOccurrences = MutableLiveData<MutableMap<String, Int>>()
+    private var isConnected = MutableLiveData<Boolean>()
     private val scope = CoroutineScope(Job() + Dispatchers.IO)
-    fun getWords(context: Context):MutableLiveData<MutableMap<String, Int>>?{
+    fun getWords(context: Context,isConnected:Boolean): MutableLiveData<MutableMap<String, Int>>? {
 
-        scope.launch {occurrences=
-            Injector.provideRepo(context).getWords(checkNetworkConnection(context)) as HashMap<String, Int>
+        scope.launch {
+            occurrences =
+                Injector.provideRepo(context)
+                    .getWords(isConnected) as HashMap<String, Int>
             liveOccurrences.postValue(occurrences)
         }
         return liveOccurrences
     }
-    private fun checkNetworkConnection(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    fun checkNetworkConnection(context: Context): LiveData<Boolean> {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val nw      = connectivityManager.activeNetwork ?: return false
-            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            val nw = connectivityManager.activeNetwork
+            if (nw == null) {
+                isConnected.postValue(false)
+                return isConnected
+            }
+            val actNw = connectivityManager.getNetworkCapabilities(nw)
+            if (actNw == null) {
+                isConnected.postValue(false)
+                return isConnected
+            }
             return when {
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    isConnected.postValue(true)
+                    isConnected
+                }
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    isConnected.postValue(true)
+                    isConnected
+                }
                 //for other device how are able to connect with Ethernet
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    isConnected.postValue(true)
+                    isConnected
+                }
                 //for check internet over Bluetooth
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
-                else -> false
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> {
+                    isConnected.postValue(true)
+                    isConnected
+                }
+
+                else -> {
+                    isConnected.postValue(false)
+                    isConnected
+                }
             }
         } else {
-            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
-            return nwInfo.isConnected
+            val nwInfo = connectivityManager.activeNetworkInfo
+            if (nwInfo == null)  {
+                isConnected.postValue(false)
+                return isConnected
+            }
+            isConnected.postValue(nwInfo.isConnected)
+            return isConnected
         }
+
+
     }
 
+    fun observeConnectivity(): LiveData<Boolean> {
+        return isConnected
+    }
 }
