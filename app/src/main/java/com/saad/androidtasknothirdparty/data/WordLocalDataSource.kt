08ccd25aa69ff87ccb71a6574898aa017ace6bb3
Ahmed Saad <asibrahim@ntgclarity.com>
2020-11-21 17:44:47 +0200

    retrieve data from database in local datasource

--- app/src/main/java/com/saad/androidtasknothirdparty/data/WordLocalDataSource.kt
+++ app/src/main/java/com/saad/androidtasknothirdparty/data/WordLocalDataSource.kt
@@ -1,9 +1,10 @@
 package com.saad.androidtasknothirdparty.data
 
 import android.content.Context
+import com.saad.parsexmlbyhttpclientsqlliteapp.db.WordsControl
 
 class WordLocalDataSource(val context: Context) {
     fun getWordsFromDatabase(): MutableMap<String, Int>? {
-        return null
+        return WordsControl(context).read()
     }
 }
