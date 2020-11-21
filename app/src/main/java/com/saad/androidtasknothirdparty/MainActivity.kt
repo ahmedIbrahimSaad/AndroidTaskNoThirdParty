0a69905aeb6f17857f2a54125babd914c1fd0af4
Ahmed Saad <asibrahim@ntgclarity.com>
2020-11-21 17:07:48 +0200

    add progress loading

--- app/src/main/java/com/saad/androidtasknothirdparty/MainActivity.kt
+++ app/src/main/java/com/saad/androidtasknothirdparty/MainActivity.kt
@@ -2,10 +2,29 @@ package com.saad.androidtasknothirdparty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rv_words: RecyclerView
+    private lateinit var progress_bar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+    fun initViews() {
+        rv_words = findViewById(R.id.rv_words)
+        progress_bar = findViewById(R.id.progress_bar)
+    }
+    fun hideLoading() {
+        progress_bar.isIndeterminate = false
+        progress_bar.visibility = View.GONE
+    }
+
+    fun showLoading() {
+        progress_bar.isIndeterminate = true
+        progress_bar.visibility = View.VISIBLE
     }
 }
