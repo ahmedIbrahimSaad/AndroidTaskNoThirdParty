d9bce9780cdcc1547903688625ea67ca0f5c2260
Ahmed Saad <asibrahim@ntgclarity.com>
2020-11-21 17:32:20 +0200

    add coroutines dependency and add network and internet stat permission

--- app/src/main/java/com/saad/androidtasknothirdparty/MainViewModel.kt
+++ app/src/main/java/com/saad/androidtasknothirdparty/MainViewModel.kt
@@ -4,13 +4,33 @@ import android.app.Application
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.*
 import com.saad.androidtasknothirdparty.di.Injector
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 
 class MainViewModel : ViewModel() {
+    var occurrences  = HashMap<String, Int>()
+    private var liveOccures= MutableLiveData<MutableMap<String,Int>>()
+    private val scope = CoroutineScope(Job() + Dispatchers.IO)
+    fun getWords(context: Context):MutableLiveData<MutableMap<String,Int>>?{
 
+        scope.launch {occurrences=
+            Injector.provideRepo(context).getWords(checkNetworkConnection(context)) as HashMap<String, Int>
+            liveOccures.postValue(occurrences)
+        }
+        return liveOccures
+    }
+    private fun checkNetworkConnection(context: Context): Boolean {
+        val cm: ConnectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = cm.activeNetworkInfo
+        val isConnected: Boolean = if(networkInfo != null) networkInfo.isConnected() else false
+        return isConnected;
+    }
 
 
 }
