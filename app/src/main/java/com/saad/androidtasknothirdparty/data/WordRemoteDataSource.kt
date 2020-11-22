a648d0c304118e6765fed877db6de79af2ac0b36
Ahmed Saad <asibrahim@ntgclarity.com>
2020-11-22 14:51:35 +0200

    make StringUtil.kt obect

--- app/src/main/java/com/saad/androidtasknothirdparty/data/WordRemoteDataSource.kt
+++ app/src/main/java/com/saad/androidtasknothirdparty/data/WordRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.saad.androidtasknothirdparty.data
 
 import com.saad.androidtasknothirdparty.R
+import com.saad.androidtasknothirdparty.StringUtil
 import com.saad.androidtasknothirdparty.db.WordsControl
 import com.saad.androidtasknothirdparty.di.Injector
 
@@ -8,7 +9,7 @@ class WordRemoteDataSource {
     private var occurrences = HashMap<String, Int>()
     fun getWordsFromNetwork(): MutableMap<String, Int>? {
         Injector.provideNetworkUtil().httpGet(Injector.provideContext().getString(R.string.url))?.let { occurrences =
-            Injector.provideStringUtils().splitTextBySpace(it) as HashMap<String, Int>
+            StringUtil.splitTextBySpace(it) as HashMap<String, Int>
         }
         WordsControl(Injector.provideContext()).create(occurrences)
         return occurrences
