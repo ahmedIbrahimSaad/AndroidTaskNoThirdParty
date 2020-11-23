6894589167a07f5f816b7eb49677e36d622cecc4
Ahmed Saad <asibrahim@ntgclarity.com>
2020-11-23 12:33:16 +0200

    handle error response

--- app/src/main/java/com/saad/androidtasknothirdparty/NetWorkUtil.kt
+++ app/src/main/java/com/saad/androidtasknothirdparty/NetWorkUtil.kt
@@ -8,27 +8,39 @@ import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
 class NetWorkUtil {
-     fun httpGet(myURL: String?)  : String?  {
+    fun httpGet(myURL: String?): String? {
 
         val inputStream: InputStream
+        val errorInputStream: InputStream
         // create URL
         val url = URL(myURL)
         // create HttpURLConnection
         val conn: HttpsURLConnection = url.openConnection() as HttpsURLConnection
+
         // make GET request to the given URL
-        conn.connect()
-        // receive response as inputStream
-        inputStream = conn.inputStream
-        // convert input stream to string
 
-        var result = ""
-        if (inputStream != null)
-            result = convertInputStreamToString(inputStream)
+        try {
+            conn.connect()
+            errorInputStream=conn.errorStream
+            Log.d("errorInputStream", errorInputStream.toString())
 
-        Log.d("html", result)
+            inputStream = conn.inputStream
+            // convert input stream to string
 
-        return result
+            var result = ""
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream)
+
+            Log.d("html", result)
+
+            return result
+        }catch (t:Exception){
+            Log.d("errorInputStream", t.message)
+            return t.message
+        }
+
     }
+
     private fun convertInputStreamToString(inputStream: InputStream): String {
         val bufferedReader: BufferedReader? = BufferedReader(InputStreamReader(inputStream))
 
